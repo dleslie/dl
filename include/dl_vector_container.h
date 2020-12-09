0cd17dbfe20939059a08706e52799ee963053ceb
Dan Leslie <dan@ironoxide.ca>
2020-12-08 19:56:39 -0800

    Faster validity check

--- include/dl_vector_container.h
+++ include/dl_vector_container.h
@@ -75,8 +75,8 @@ dl_ptr _vector_container_pop(dl_ptr c, dl_ptr out) {
 }
 
 dl_bool _vector_container_iterator_valid(dl_iterator i) {
-  dl_natural length = dl_vector_length((dl_vector *)((dl_container *)i.container)->storage);
-  return i.data.index >= 0 && i.data.index < length;
+  dl_vector *v = (dl_vector *)((dl_container *)i.container)->storage;
+  return i.data.index >= 0 && dl_safety(v != NULL) && i.data.index < v->length;
 }
 
 dl_ptr _vector_container_iterator_get(dl_iterator i, dl_ptr out) {
