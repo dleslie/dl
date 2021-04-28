b64a45e16baf012c3ab3bbc6d8577a38e84ecbb1
Dan Leslie <dan@ironoxide.ca>
2021-04-27 23:19:17 -0700

    stubbed out base behaviour

--- include/dl_memory.h
+++ include/dl_memory.h
@@ -11,8 +11,23 @@ dl_api dl_ptr dl_memory_swap(dl_ptr target, dl_ptr source, dl_natural dl_bytes);
 dl_api dl_ptr dl_memory_copy(dl_ptr target, dl_ptr source, dl_natural dl_bytes);
 dl_api dl_ptr dl_memory_set(dl_ptr target, dl_byte val, dl_natural dl_bytes);
 
+typedef void *(*dl_memory_allocator)(size_t sz);
+typedef void (*dl_memory_releaser)(void *ptr);
+
+struct dl_memory_manager;
+struct dl_memory_manager {
+  dl_memory_allocator alloc;
+  dl_memory_releaser free;
+  struct dl_memory_manager *_next;
+};
+typedef struct dl_memory_manager dl_memory_manager;
+
 extern void *(*dl_alloc)(size_t sz);
 extern void (*dl_free)(void *ptr);
+
+dl_api dl_ptr dl_push_memory_manager(dl_memory_manager *manager);
+dl_api dl_ptr dl_pop_memory_manager();
+extern
 
 #if defined(__cplusplus)
 }
@@ -26,12 +41,37 @@ extern void (*dl_free)(void *ptr);
 
 #if defined(DL_USE_MALLOC)
 #include <malloc.h>
+dl_memory_manager root_allocator = {(dl_memory_allocator)malloc, (dl_memory_releaser)free, NULL};
 void *(*dl_alloc)(size_t) = malloc;
 void (*dl_free)(void *) = free;
 #else
-void *dl_alloc(size_t sz) { return 0; }
-void dl_free(void *location) {}
+void *__dl_alloc(size_t sz) { return 0; }
+void __dl_free(void *location) {}
+dl_memory_manager root_allocator = {(dl_memory_allocator)__dl_alloc, (dl_memory_releaser)__dl_free, NULL};
+void *(*dl_alloc)(size_t) = __dl_alloc;
+void (*dl_free)(void *) = __dl_free;
 #endif
+
+dl_memory_manager *current_allocator = &root_allocator;
+
+dl_api dl_ptr dl_push_memory_manager(dl_memory_manager *manager) {
+  if (manager == NULL) return NULL;
+  manager->_next = current_allocator;
+  current_allocator = manager;
+  dl_alloc = manager->alloc;
+  dl_free = manager->free;
+  return (dl_api)current_allocator;
+}
+
+dl_api dl_ptr dl_pop_memory_manager() {
+  dl_ptr popped;
+  if (current_allocator->_next == NULL) return NULL;
+  popped = current_allocator;
+  current_allocator = current_allocator->_next;
+  dl_alloc = current_allocator->alloc;
+  dl_free = current_allocator->free;
+  return popped;
+}
 
 dl_api dl_ptr dl_memory_swap(dl_ptr left, dl_ptr right, dl_natural dl_bytes) {
   size_t sz_count, byte_count, *sz_left, *sz_right, sz_temp;
